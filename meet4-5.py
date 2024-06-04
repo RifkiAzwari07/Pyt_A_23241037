3c1cf83709eb73e148cc1271674c94390ee3a2dc
RifkiAzwari07 <145946605+RifkiAzwari07@users.noreply.github.com>
2024-06-04 21:08:47 +0800

    Add files via upload

---
+++ meet4-5.py
@@ -0,0 +1,52 @@
+ # inputan
+ # Aritmatika + - x :
+  
+
+#x = 5
+#coba = input("masukan nama :")
+# print(coba)
+
+#bilangan random = input("masukan bilangan acak :")
+#ptint bilangan random
+
+#ARITMATIKA
+x = 9
+y = 3
+
+#penjumlahan +
+hasil = x + y 
+print(x, '+' , y, '=', hasil)
+
+#pengurangan -
+hasil = x - y 
+print(x, '-' , y, '=', hasil)
+
+#perkalian *
+hasil = x * y 
+print(x, 'x' , y, '=', hasil)
+
+#pembagian /
+hasil = x / y 
+print(x, ':' , y, '=', hasil)
+
+# perpangkatan **
+hasil = x ** y 
+print(x, '^' , y, '=', hasil)
+
+# modulus %
+hasil = x % y 
+print(x, 'mod' , y, '=', hasil)
+
+# floor division //
+hasil = x // y 
+print(x, 'Floor' , y, '=', hasil)
+
+#skala prioritas (), **, */:, +/-
+x = 4
+y = 7
+z = 5
+hasil = ( x ** y * (z + x) / y - z % x // y)
+print(hasil)
+print( x, '**', y,  '*',z, '+', x, '/', y,  '-', z,  '%', x, '//', y,)
+
+
