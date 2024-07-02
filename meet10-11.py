29c64c81158ad93ef206103e01f7d8fd44a75998
RifkiAzwari07 <145946605+RifkiAzwari07@users.noreply.github.com>
2024-07-02 08:31:48 +0800

    Add files via upload

---
+++ meet10-11.py
@@ -0,0 +1,22 @@
+#belajar perulangan (loop)
+
+bil = 1
+print(bil)
+bil = bil + 2
+print(bil)
+bil = bil + 2
+print(bil)
+bil = bil + 2
+print(bil)
+
+#for kondisi :
+# aksi
+
+# penggunaan loop pada pemograman lain 
+# for (int i; i<=)
+
+bil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(bil)
+
+for i in bil :
+    print(f"nilai i sekarang -> {i}")
